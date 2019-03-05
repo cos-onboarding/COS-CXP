e7f52cb67067e90cf9cc92093818326ac26208a6
unknown <517360666@qq.com>
2019-03-05 15:08:02 +0800

    add properties reader

---
+++ services/hase-cos-dashboard/src/main/java/com/hase/cos/properties/CosPropertyConfiguration.java
@@ -0,0 +1,33 @@
+package com.hase.cos.dashboard.properties;
+
+import java.util.Properties;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CosPropertyConfiguration extends PropertyPlaceholderConfigurer {
+	
+	private Properties props;      
+
+    @Override
+    protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, Properties props)
+                            throws BeansException {
+        super.processProperties(beanFactoryToProcess, props);
+        this.props = props;
+    }
+
+    public String getProperty(String key){
+        return this.props.getProperty(key);
+    }
+
+    public String getProperty(String key, String defaultValue) {
+        return this.props.getProperty(key, defaultValue);
+    }
+
+    public Object setProperty(String key, String value) {
+        return this.props.setProperty(key, value);
+    }
+}
