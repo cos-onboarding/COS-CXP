1d7bb80b01efbfbcca43de2eb7cbcbce1fadd6a7
GyLeonardo <gyfz0422@163.com>
2019-03-06 16:54:59 +0800

    zach tj

---
+++ services/hase-cos-dashboard/src/main/java/com/hase/cos/dashboard/CalendarProcessor.java
@@ -0,0 +1,25 @@
+package com.hase.cos.dashboard;
+
+import com.hase.cos.dashboard.Util.CamelProcessorUtils;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+
+@Component("calendarProcessor")
+public class CalendarProcessor implements Processor {
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        System.out.println("<============================== Calendar ================================>");
+        InputStream body = null;
+        body = exchange.getIn().getBody(InputStream.class);
+        String data = CamelProcessorUtils.setHttpBody(body);
+
+        exchange.getOut().setHeader("content-type", "application/json");
+
+        exchange.getOut().setBody(data);
+
+    }
+}
--- statics/collection/hase-cos/widgets/widget-hase-cos-application-list/scripts/controllers/application-ctrl.js
+++ statics/collection/hase-cos/widgets/widget-hase-cos-application-list/scripts/controllers/application-ctrl.js
@@ -11,7 +11,7 @@ define(function (require, exports, module) {
      * @ngInject
      * @constructor
      */
-    function ApplicationCtrl(model, lpWidget, lpCoreUtils,$rootScope,$scope) {
+    function ApplicationCtrl(model, lpWidget, lpCoreUtils,$rootScope,$scope,$http) {
         this.state = model.getState();
         this.utils = lpCoreUtils;
         this.widget = lpWidget;
@@ -21,10 +21,47 @@ define(function (require, exports, module) {
 
     ApplicationCtrl.prototype.$onInit = function() {
         // Do initialization here
+        this.$scope.applicationNumber = '';
+        this.$scope.customerId = '';
+        this.$scope.customerName = '';
+        this.$scope.statusList = [];
+        this.$scope.businessCenterList = [];
+        this.$scope.staffAssignedList = [];
+        this.$scope.summaryStatusList = [{
+            "status":'1111',"statusCount":2
+        },{"status":'2222',"statusCount":433}];
     };
     ApplicationCtrl.prototype.click = function() {
         // Do initialization here
         this.$rootScope.$state.go('C2');
     };
+    /**
+     * Zach Y Gao
+     * 05-03-2019
+     * @param status
+     * ** filter applications list data by state ** view button
+     */
+    ApplicationCtrl.prototype.statusApplicationListButton = function(status){
+        this.$scope.fileStatusData = status;
+    };
+
+    ApplicationCtrl.prototype.caseSearchButton = function(){
+        var params = {
+            "applicationNumber": this.$scope.applicationNumber,
+            "customerId": this.$scope.customerId,
+            "customerName": this.$scope.customerName,
+            "status": "statusName",
+            "businessCenter": "businessName",
+            "staffAssigned": "staffName"
+        }
+        $http.post("http://,", $scope.formData)
+                    .success(function(result){
+                     })
+                    .error(function(result){
+                     });
+
+        this.$scope.fileStatusData = '';
+    };
+
     module.exports = ApplicationCtrl;
 });
