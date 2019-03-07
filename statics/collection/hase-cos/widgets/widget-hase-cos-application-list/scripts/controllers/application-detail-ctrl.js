cf2f07565337737e9f0f4e57a85f917a08db6d4c
bhhglr <277212547@qq.com>
2019-03-07 18:54:45 +0800

    ben check

--- statics/collection/hase-cos/widgets/widget-hase-cos-application-list/scripts/controllers/application-detail-ctrl.js
+++ statics/collection/hase-cos/widgets/widget-hase-cos-application-list/scripts/controllers/application-detail-ctrl.js
@@ -27,23 +27,44 @@ define(function (require, exports, module) {
     }
 
     ApplicationDetailCtrl.prototype.$onInit = function() {
-        console.log("2222222222"+this.$rootScope.$stateParams);
-        console.log("2222222222"+this.$rootScope.$stateParams);
+        var applicationDetailCtrl = this;
         // Do initialization here
         this.$scope.id = this.$stateParams.id;
+        this.$scope.roleId = "";
         console.log(this.$scope.id);
         this.$scope.reject = false;
         this.$scope.fullUserName = "Chrismrs Wong";
         this.$scope.simpleUserName = "CW";
         this.$scope.selectedLanguage = "English";
-        var data = {};
-        this.commonService.getCommonMessage(data).then(
+        this.$scope.appliDetails =[];
+        var data = {
+            applicationId:applicationDetailCtrl.$scope.id,
+            roleId: applicationDetailCtrl.$scope.roleId,
+            url:'/applicationDetailSeach'
+        };
+
+        //return applicationDetail
+        this.commonService.getCommonServiceMessage(data).then(
             function(response){
-
+                applicationDetailCtrl.$scope.appliDetails = response.data
             },function(){
-
+                applicationDetailCtrl.$rootScope.$state.go('C');
             }
-        )
+        );
+        var dataCheck = {
+            applicationId:applicationDetailCtrl.$scope.id,
+            roleId: applicationDetailCtrl.$scope.roleId,
+            url:'/applicationCheckSeach'
+        };
+
+        //return checkList
+        this.commonService.getCommonServiceMessage(dataCheck).then(
+            function(response){
+                applicationDetailCtrl.$scope.appliDetails = response.data
+            },function(){
+                applicationDetailCtrl.$rootScope.$state.go('C');
+            }
+        );
         this.$scope.checklists = [
             {"id":"1","txt":"Booked Time Slot"},
             {"id":"2","txt":"Booked Ssh Slot"},
@@ -51,13 +72,13 @@ define(function (require, exports, module) {
             {"id":"4","txt":"Booked Fun Slot"},
             {"id":"5","txt":"Booked Shi Slot"}
         ];
-        this.$scope.appliDetails = [
-            {"title":"Status","content":"Pedding"},
-            {"title":"Appointment","content":"31 Dec 2020, 13:30 Central"},
-            {"title":"Appointment","content":"31 Dec 2020, 13:30 Central"},
-            {"title":"Assigned to:","content":"Booked Fun Slot"},
-            {"title":"Assigned to:","content":"Booked Shi Slot"}
-        ];
+        // this.$scope.appliDetails = [
+        //     {"title":"Status","content":"Pedding"},
+        //     {"title":"Appointment","content":"31 Dec 2020, 13:30 Central"},
+        //     {"title":"Appointment","content":"31 Dec 2020, 13:30 Central"},
+        //     {"title":"Assigned to:","content":"Booked Fun Slot"},
+        //     {"title":"Assigned to:","content":"Booked Shi Slot"}
+        // ];
         //checkList select data
         this.$scope.selected = [] ; 
         this.$scope.isAllCheck = true;
