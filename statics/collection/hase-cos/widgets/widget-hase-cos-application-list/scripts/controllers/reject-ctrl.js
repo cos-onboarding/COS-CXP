280b41ce8fe9ecb2aaeeb78a2b9afbf04ae4a6b1
william <williamlfee@163.com>
2019-03-20 11:29:53 +0800

    fix

--- statics/collection/hase-cos/widgets/widget-hase-cos-application-list/scripts/controllers/reject-ctrl.js
+++ statics/collection/hase-cos/widgets/widget-hase-cos-application-list/scripts/controllers/reject-ctrl.js
@@ -11,7 +11,7 @@ define(function (require, exports, module) {
      * @ngInject
      * @constructor
      */
-    function RejectCtrl(model, lpWidget, lpCoreUtils,$rootScope,$scope,commonService,$stateParams) {
+    function RejectCtrl(model, lpWidget, lpCoreUtils,$rootScope,$scope,commonService,$stateParams,$http) {
 
 
         this.state = model.getState();
@@ -21,6 +21,8 @@ define(function (require, exports, module) {
         this.$scope = $scope;  
         this.commonService =  commonService; 
         this.$stateParams = $stateParams;
+        this.$http = $http;
+        this.serviceUrl = lpWidget.getPreference("serviceUrl");
 
         
     }
@@ -64,7 +66,35 @@ define(function (require, exports, module) {
     //confirm
     RejectCtrl.prototype.submit = function(){
         var rejectCtrl = this;
+
         var time = this.$scope.dateTime;
+        var staff_id = this.$scope.staff_id;
+        var reCategory = this.$scope.province;
+        var emCategory = this.$scope.provinceTwo;
+        var rejectReason = this.$scope.rejectReason;
+        var isEnhanced = this.$scope.isEnhanced ? 'Yes' : 'No';
+        var isNexus = this.$scope.isNexus ? 'Yes' : 'No';
+        //console.log(time+"=="+staff_id+"=="+reCategory+"=="+emCategory+"=="+rejectReason+"=="+isEnhanced+"=="+isNexus);
+        var param = {
+            Appcation_ID:this.$scope.Appcation_ID,
+            reject_date:time,
+            reject_staff:staff_id,
+            reject_category:reCategory,
+            reject_email:emCategory,
+            reject_reason:rejectReason,
+            isEnhanced:isEnhanced,
+            isNexus:isNexus   
+        }
+        console.log(param);
+
+        rejectCtrl.$http.post(rejectCtrl.serviceUrl+"/saveRejectReason",param)
+        .then(function(response){
+
+        });
+
+
+
+
     }
 
 
